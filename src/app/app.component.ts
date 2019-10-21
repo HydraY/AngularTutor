313d6b2077435970ca0d3e71d6adc5c266bc53c1
unknown <interhydra@gmail.com>
2019-10-20 20:00:57 -0700

    hw3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.hw2.html',
-  styleUrls: ['./app.component.hw2.css']
+  templateUrl: './app.component.hw3.html',
+  styleUrls: ['./app.component.hw3.css']
 })
 export class AppComponent {
   title = '九章全栈基础课!';
